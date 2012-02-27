6b9ccb99f44cf417f4f6290bccd4e55284f29a85
Maarten Billemont <lhunath@lyndir.com>
2012-02-27 11:59:56 +0100

    Pearl prefixing update.

--- cocos2d/CCTextureCache.m
+++ cocos2d/CCTextureCache.m
@@ -35,7 +35,7 @@
 #import "Support/CCFileUtils.h"
 #import "CCDirector.h"
 #import "ccConfig.h"
-#import "UIImage_Scaling.h"
+#import "UIImage_PearlScaling.h"
 
 // needed for CCCallFuncO in Mac-display_link version
 #import "CCActionManager.h"
